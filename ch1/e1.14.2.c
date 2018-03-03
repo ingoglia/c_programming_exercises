7c7303a5a1811424f789a965cf498e4ee63cc9f5
Dominique Ingoglia <ingoglia@protonmail.com>
2018-03-02 22:56:21 -0700

    adding 1-14 attempts

---
+++ ch1/e1.14.2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+   int c;
+   int pairc[100]; 
+   
+   for (i = 0; i < 20; ++i)
+      pairc[i] =   
+   while((c = getchar()) != EOF) {
+    for (i = 0; i <= k; ++i){ 
+      if (c != wordc[i]){
+         ++k;
+         
+     
+/* i want to make a program that takes in a set of characters and gathers a list of each one. 
---
+++ ch1/e1.14.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+//print a histogram of the frequencies of different characters in its input
+
+   int c, i, n;
+   int wordc[], countc[];
+   
+   n = 0;   
+   while((c = getchar()) != EOF) {
+      for (i = 0; i < n; ++i)
+          if (c == wordc[i] {
+             ++countc[i];
+          else  if (c != wordc[i]) {
+             ++n;
+             let wordc[i] = c
+            //let ++countc[i]
+            }
+	 else if ( c != wordc[i] {
+            let ++countc[i]         
+   }
+
+}
