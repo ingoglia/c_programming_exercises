6a63fc2d957a6d629288d509fc0c25603e9fa4d1
Dominique Ingoglia <ingoglia@protonmail.com>
2018-03-04 16:20:55 -0700

    add completed 1-14

--- ch1/e1.14.c
+++ ch1/e1.14.c
@@ -3,24 +3,40 @@
 int main(){
 //print a histogram of the frequencies of different characters in its input
 
-   int c, i, j, n;
-   int wordc[100], countc[100];
-  
-   for (i = 0; i <= 100; ++i)
-        wordc[i] = countc[i] = 0; 
-
-   while((c = getchar()) != EOF) {
-      for (j = 0; j < 101; ++j){
-         if (c == wordc[j]){
-             ++countc[j];
-          } 
-         else if ((c != wordc[i]) && (i > 0) && (wordc[i] == '0')) {
-             wordc[j] = c;
-             ++countc[j];
-          }
-            }
+   int c, i, j, n, t;
+   int countc[100];
+   char wordc[100];
+   for (i = 0; i <= 100; ++i){
+        wordc[i] = '0';
+        countc[i] = 0; 
    }
-   for (n = 0; n <= wordc[98]; ++n)
-      printf("%c ", wordc[n+1]);
-}
 
+   while((c = getchar()) != EOF) { 
+      if (c == '0'){ //account for the 0 element
+        printf("in zero");
+        countc[1]++;
+      }
+      if (c != '0'){ 
+         for (j = 2; j <= 100; ++j){
+            if (c == wordc[j]){ //if the char exists in the list count it
+             ++countc[j];
+             break;
+             } 
+            else if ((c != '\n') && (c != wordc[j]) && (wordc[j] == '0') && (wordc[j-1] != 'c')){ //add the char to the list and count it
+               wordc[j] = c;
+               ++countc[j];
+               break;
+            }
+         }
+      }
+    }
+   printf("\nHistogram for frequency of words of a given length. \n");
+   for (n = 0; n < 101; ++n){
+     if (countc[n] != 0){
+        printf("%c   ", wordc[n]);
+        for(t = 0; t < countc[n]; ++t)
+           printf("[]");
+        printf("\n");
+     }
+   }
+}
