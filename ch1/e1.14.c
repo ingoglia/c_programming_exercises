530b886418b536aec3361d43a316aa5b687d1dcc
Dominique Ingoglia <ingoglia@protonmail.com>
2018-03-03 23:22:21 -0700

    add to 1-14

--- ch1/e1.14.c
+++ ch1/e1.14.c
@@ -3,21 +3,24 @@
 int main(){
 //print a histogram of the frequencies of different characters in its input
 
-   int c, i, n;
-   int wordc[], countc[];
-   
-   n = 0;   
-   while((c = getchar()) != EOF) {
-      for (i = 0; i < n; ++i)
-          if (c == wordc[i] {
-             ++countc[i];
-          else  if (c != wordc[i]) {
-             ++n;
-             let wordc[i] = c
-            //let ++countc[i]
-            }
-	 else if ( c != wordc[i] {
-            let ++countc[i]         
-   }
+   int c, i, j, n;
+   int wordc[100], countc[100];
+  
+   for (i = 0; i <= 100; ++i)
+        wordc[i] = countc[i] = 0; 
 
+   while((c = getchar()) != EOF) {
+      for (j = 0; j < 101; ++j){
+         if (c == wordc[j]){
+             ++countc[j];
+          } 
+         else if ((c != wordc[i]) && (i > 0) && (wordc[i] == '0')) {
+             wordc[j] = c;
+             ++countc[j];
+          }
+            }
+   }
+   for (n = 0; n <= wordc[98]; ++n)
+      printf("%c ", wordc[n+1]);
 }
+
