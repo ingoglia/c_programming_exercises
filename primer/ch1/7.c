4ddb85c1395646099fb1c86a94cb7ebeb35fff1f
utena <mrnobody@debian.space>
2018-07-22 14:00:01 -0600

    completed ch2

---
+++ primer/ch1/7.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void s(void);
+
+int main(void)
+{
+  s();
+	s();
+	s();
+  printf("\n");
+	s();
+	s();
+  printf("\n");
+  s();
+  printf("\n");
+  return 0;
+}
+
+void s(void)
+{
+  printf("Smile!");
+}
+
+  
